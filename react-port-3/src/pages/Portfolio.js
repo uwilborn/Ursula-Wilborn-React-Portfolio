c98bc982f60a07158337ec61a698511fbeb825f2
Ursula Wilborn <wilbornu@yahoo.com>
2021-10-02 22:08:46 -0400

    update style

--- react-port-3/src/pages/Portfolio.js
+++ react-port-3/src/pages/Portfolio.js
@@ -1,7 +1,54 @@
 import React from "react";
 
 function Portfolio() {
-  return <div></div>;
+  let projects = [
+    {
+      name: "Password Generator",
+      github: "https://github.com/uwilborn/Password-Generator",
+      live: "https://uwilborn.github.io/Password-Generator/",
+      image: "/images/Password.png",
+    },
+    {
+      name: "Password Generator",
+      github: "https://github.com/uwilborn/Password-Generator",
+      live: "https://uwilborn.github.io/Password-Generator/",
+      image: "/images/Password.png",
+    },
+    {
+      name: "Password Generator",
+      github: "https://github.com/uwilborn/Password-Generator",
+      live: "https://uwilborn.github.io/Password-Generator/",
+      image: "/images/Password.png",
+    },
+  ];
+  return (
+    <div className="d-flex flex-wrap justify-content-between">
+      {projects.map((project, key) => (
+        <div className="card" style={{ width: "33%;" }}>
+          <img
+            src={project.image}
+            className="card-img-top"
+            width={200}
+            height={200}
+            alt={key}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{project.name}</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href={project.github} className="btn btn-primary">
+              Github
+            </a>
+            <a href={project.live} className="btn btn-primary">
+              Live
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Portfolio;
