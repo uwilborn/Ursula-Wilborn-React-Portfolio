eeee726e4dffd4396c45a5974a11c92177179fa2
Ursula Wilborn <wilbornu@yahoo.com>
2021-10-03 22:00:59 -0400

    Update links

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,19 +3,16 @@ import React from "react";
 function Contact() {
   return (
     <div className="bg-warning container d-flex align-items-center justify-content-center">
-      <h3>
-        <a href="">Linked In</a>
+      <h3 className="m-3 p-3">
+        <a href="https://www.linkedin.com/in/ursula-w-2610ba186">Linked In</a>
       </h3>
-      <h3>
-        <a href="">Github</a>
+      <h3 className="m-3 p-3">
+        <a href="https://github.com/uwilborn">Github</a>
       </h3>
-      <h3>
+      <h3 className="m-3 p-3">
         <a href="https://docs.google.com/document/d/154VOVhtGL4dTrgcqTYNceUP_HTuzf8ZB/edit?usp=sharing&ouid=114708946162489537944&rtpof=true&sd=true">
           Resume
         </a>
-      </h3>
-      <h3>
-        <a href=""></a>
       </h3>
     </div>
   );
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,22 +3,142 @@ import React from "react";
 function Portfolio() {
   let projects = [
     {
-      name: "Password Generator",
+      name: "HTML, CSS, and Git: Code Refactor",
+      github: "https://github.com/uwilborn/Horiseon-Improved-Accessibility",
+      live: "https://uwilborn.github.io/Horiseon-Improved-Accessibility/",
+      image: "/images/CodeRefactor.png",
+    },
+    {
+      name: "Portfolio",
+      github: "https://github.com/uwilborn/Ursula-Wilborn-Portfolio.git",
+      live: "https://uwilborn.github.io/Ursula-Wilborn-Portfolio/",
+      image: "/images/Portfolio1.png",
+    },
+    {
+      name: "JavaScript: Password Generator",
+      github: "https://github.com/uwilborn/Password-Generator",
+      live: "https://uwilborn.github.io/Password-Generator/",
+      image: "/images/Password.PNG",
+    },
+    {
+      name: "Web APIs: Code Quiz",
+      github: "https://github.com/uwilborn/Challenge-Coding-Quiz.git",
+      live: "https://uwilborn.github.io/Challenge-Coding-Quiz/",
+      image: "/images/CodeQuiz.png",
+    },
+    {
+      name: "Third-Party APIs: Work Day Scheduler",
+      github: "https://github.com/uwilborn/Workday-Schedule-App.git",
+      live: "https://uwilborn.github.io/Workday-Schedule-App/",
+      image: "/images/Scheduler.png",
+    },
+    {
+      name: "Server-Side APIs: Weather Dashboard",
+      github: "https://github.com/uwilborn/Funky-Weather-Dashboard.git",
+      live: "https://uwilborn.github.io/Funky-Weather-Dashboard/",
+      image: "/images/Weather.png",
+    },
+    {
+      name: "Project 1",
+      github: "https://github.com/Xtended99/CoffeeOverFlow.git",
+      live: "https://xtended99.github.io/CoffeeOverFlow/",
+      image: "/images/Project1.png",
+    },
+    {
+      name: "Portfolio 2",
+      github: "https://github.com/uwilborn/Ursula-Wilborn-Portfolio.git",
+      live: "https://uwilborn.github.io/Ursula-Wilborn-Portfolio/",
+      image: "/images/Portfolio2.png",
+    },
+    {
+      name: "Node.js Homework: Professional README Generator",
+      github: "https://github.com/uwilborn/RightOn-README-Generator",
+      live: "https://drive.google.com/file/d/1H_Uu6NS3Lq83uoJIrcaKy0tx6CClaPji/view",
+      image: "/images/README.png",
+    },
+    {
+      name: "Object-Oriented Programming: Team Profile Generator",
+      github: "https://github.com/uwilborn/Dynamic-Team-Generator.git",
+      live: "https://drive.google.com/file/d/1S5mc3eiRhXpBaTI9w8CBPK8QBQ-TNqJt/view",
+      image: "/images/Team.png",
+    },
+    {
+      name: "Express.js: Note Taker",
+      github: "https://github.com/uwilborn/Star-Note-Taker",
+      live: "https://starnotetaker.herokuapp.com/",
+      image: "/images/Notetaker.png",
+    },
+    {
+      name: "MySQL Homework: Employee Tracker",
+      github: "https://github.com/uwilborn/QA-Employee-Tracker",
+      live: "https://drive.google.com/file/d/1Ah637tYqSClUBYg3DkBO4I2HMdL7SmWx/view",
+      image: "/images/ETracker.png",
+    },
+    {
+      name: "Object-Relational Mapping (ORM): E-Commerce Back End",
+      github: "https://github.com/uwilborn/Cinnamon-Commerce",
+      live: "https://watch.screencastify.com/v/9uZ8VevsTLMGODTAvykb",
+      image: "/images/ECommerce.png",
+    },
+    {
+      name: "Model-View-Controller (MVC): Tech Blog",
+      github: "https://github.com/uwilborn/Password-Generator",
+      live: "https://uwilborn.github.io/Password-Generator/",
+      image: "/images/Tech.png",
+    },
+    {
+      name: "Project 2: Interactive Full-Stack Application",
+      github: "https://github.com/uwilborn/Password-Generator",
+      live: "https://uwilborn.github.io/Password-Generator/",
+      image: "/images/Hub.png",
+    },
+    {
+      name: "Project 2: Professional Materials",
+      github: "https://github.com/uwilborn/The-Hub",
+      live: "https://watch.screencastify.com/v/BynT4HR1cjdahniVy4Hl",
+      image: "/images/Portfolio3.png",
+    },
+    {
+      name: "Computer Science for JavaScript: Regex Tutorial",
+      github: "https://github.com/uwilborn/Password-Generator",
+      live: "https://uwilborn.github.io/Password-Generator/",
+      image: "/images/Regex.png",
+    },
+    {
+      name: "Nosql Homework: Workout Tracker",
+      github: "https://github.com/uwilborn/HipHop-Excercise-Tracker",
+      live: "https://hiphopfitnesstracker.herokuapp.com/",
+      image: "/images/FTracker.png",
+    },
+    {
+      name: "PWA Homework: Online/Offline Budget Trackers",
       github: "https://github.com/uwilborn/Password-Generator",
       live: "https://uwilborn.github.io/Password-Generator/",
       image: "/images/Password.png",
     },
     {
-      name: "Password Generator",
+      name: "React: React Portfolio",
       github: "https://github.com/uwilborn/Password-Generator",
       live: "https://uwilborn.github.io/Password-Generator/",
       image: "/images/Password.png",
     },
     {
-      name: "Password Generator",
+      name: "MERN: Book Search Engine",
       github: "https://github.com/uwilborn/Password-Generator",
       live: "https://uwilborn.github.io/Password-Generator/",
-      image: "/images/Password.png",
+      image: "/images/Engine.png",
+    },
+    {
+      name: "State Homework: Redux Store",
+      github: "https://github.com/uwilborn/Password-Generator",
+      live: "https://uwilborn.github.io/Password-Generator/",
+      image: "/images/Redux.png",
+    },
+    {
+      name: "Final Project: MERN Stack Single-Page Application",
+      github: "https://github.com/uwilborn/Password-Generator",
+      live: "https://uwilborn.github.io/Password-Generator/",
+      image: "/images/FreeBird.png",
     },
   ];
   return (
@@ -26,18 +146,15 @@ function Portfolio() {
       {projects.map((project, key) => (
         <div className="card" style={{ width: "33%;" }}>
           <img
-            src={project.image}
             className="card-img-top"
+            src={project.image}
             width={400}
             height={400}
             alt={key}
           />
           <div className="card-body">
             <h5 className="card-title">{project.name}</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
+            <p className="card-text">Visit the Links</p>
             <a href={project.github} className="btn btn-primary">
               Github
             </a>
