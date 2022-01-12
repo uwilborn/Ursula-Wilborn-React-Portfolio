9da93597f09f7258f2c51ab234b195a9d282b3b5
Ursula Wilborn <wilbornu@yahoo.com>
2022-01-12 09:48:24 -0500

    update header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,38 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../headerstyle.css";
+import profilePicture from "../pages/images/20191012_180028.jpg";
+import displayPicture from "../pages/images/drone-3874000__340.jpg";
 
 function Header() {
   return (
     <header className="header">
-      <img src="../pages/images/Purple_2053.jpg" className="logo" />
-      <h2>
-        <Link className="" to="/">
-          <span>Ursula Wilborn</span>
-        </Link>
-      </h2>
-      <ul className="nav-links">
-        <li className="">
-          <Link className="" aria-current="page" to="/">
-            <span>Home</span>
+      <div className="">
+        <img src={profilePicture} align="left" className="headerleft" />
+        <img src={displayPicture} align="right" className="headerright" />
+      </div>
+      <div className="" align="center">
+        <h2>
+          <Link className="name" to="/">
+            <span>Ursula Wilborn</span>
           </Link>
-        </li>
-        <li className="">
-          <Link className="" to="/about">
-            <span>About</span>
-          </Link>
-        </li>
-        <li className="">
-          <Link className="" to="/portfolio">
-            <span>Portfolio</span>
-          </Link>
-        </li>
-        <li className="">
-          <Link className="" to="/contact">
-            <span>Contact</span>
-          </Link>
-        </li>
-      </ul>
+        </h2>
+      </div>
+      <div>
+        <ul className="nav-links">
+          <li className="">
+            <Link className="" aria-current="page" to="/">
+              <span>Home</span>
+            </Link>
+          </li>
+
+          <li className="">
+            <Link className="" to="/about">
+              <span>About</span>
+            </Link>
+          </li>
+          <li className="">
+            <Link className="" to="/portfolio">
+              <span>Portfolio</span>
+            </Link>
+          </li>
+          <li className="">
+            <Link className="" to="/contact">
+              <span>Contact</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 }
